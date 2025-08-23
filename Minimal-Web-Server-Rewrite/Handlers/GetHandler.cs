0b0b2225dfe27e572b3d9c3e377c2b563d5bf929
Ozzy-ZY <zyadhimself1@gmail.com>
2025-08-23 18:49:47 +0300

    Refactor: Integrate response pipeline with GetHandler and add logging middleware support

--- Minimal-Web-Server-Rewrite/Handlers/GetHandler.cs
+++ Minimal-Web-Server-Rewrite/Handlers/GetHandler.cs
@@ -1,23 +1,13 @@
 using System.Net.Sockets;
 using System.Text;
 using Minimal_Web_Server_Rewrite.Models;
+using Minimal_Web_Server_Rewrite.ResponsePipeline;
+using Minimal_Web_Server_Rewrite.ResponsePipeline.Middlewares;
 
 namespace Minimal_Web_Server_Rewrite.Handlers;
 
 public class GetHandler: IAsyncHandler
 {
-    public void HandleRequest(HttpRequest request, Socket socket)
-    {
-        var response = new HttpResponse.ResponseBuilder()
-            .WithStatusCode(HttpStatusCode.Ok)
-            .WithStatusText("OK")
-            .WithStringBody("hello world")
-            .Build();
-        
-        Console.WriteLine(response.ToString());
-        socket.Send(Encoding.UTF8.GetBytes(response.ToString()));;
-    }
-
     public async Task HandleRequestAsync(HttpRequest request, Socket socket)
     {
         var response = new HttpResponse.ResponseBuilder()
@@ -27,6 +17,9 @@ public class GetHandler: IAsyncHandler
             .Build();
         
         Console.WriteLine(response.ToString());
-        await socket.SendAsync(Encoding.UTF8.GetBytes(response.ToString()));;
+        var pipeline = new ResponsePipeline.ResponsePipeline();
+        var middleware = new LoggingMiddleware();
+        pipeline.Use(middleware);
+        await pipeline.ExecutePipelineAsync(request, response, socket);
     }
 }
